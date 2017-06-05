9e452694d78eb0d56ddb2f5b7f4ff6b43bcc8218
kyros2015 <hernan.code.zero@gmail.com>
2017-06-04 22:45:45 -0500

    FIX - clases

---
+++ sigbah/sigbah_common/src/main/java/pe/com/sigbah/common/bean/BaseOutputBean.java
@@ -0,0 +1,92 @@
+package pe.com.sigbah.common.bean;
+
+/**
+ * @className: BaseException1.java
+ * @description: 
+ * @date: 2 de jun. de 2017
+ * @author: SUMERIO.
+ */
+public class BaseOutputBean extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	private String codigoRespuesta;
+	private String mensajeRespuesta;
+	private String indicador;
+	private String url;
+
+	/**
+	 * 
+	 */
+	public BaseOutputBean() {
+		super();
+	}
+
+	/**
+	 * @param codigoRespuesta
+	 * @param mensajeRespuesta
+	 * @param indicador
+	 */
+	public BaseOutputBean(String codigoRespuesta, String mensajeRespuesta, String indicador) {
+		super();
+		this.codigoRespuesta = codigoRespuesta;
+		this.mensajeRespuesta = mensajeRespuesta;
+		this.indicador = indicador;
+	}
+
+	/**
+	 * @return the codigoRespuesta
+	 */
+	public String getCodigoRespuesta() {
+		return codigoRespuesta;
+	}
+
+	/**
+	 * @param codigoRespuesta the codigoRespuesta to set
+	 */
+	public void setCodigoRespuesta(String codigoRespuesta) {
+		this.codigoRespuesta = codigoRespuesta;
+	}
+
+	/**
+	 * @return the mensajeRespuesta
+	 */
+	public String getMensajeRespuesta() {
+		return mensajeRespuesta;
+	}
+
+	/**
+	 * @param mensajeRespuesta the mensajeRespuesta to set
+	 */
+	public void setMensajeRespuesta(String mensajeRespuesta) {
+		this.mensajeRespuesta = mensajeRespuesta;
+	}
+
+	/**
+	 * @return the indicador
+	 */
+	public String getIndicador() {
+		return indicador;
+	}
+
+	/**
+	 * @param indicador the indicador to set
+	 */
+	public void setIndicador(String indicador) {
+		this.indicador = indicador;
+	}
+
+	/**
+	 * @return the url
+	 */
+	public String getUrl() {
+		return url;
+	}
+
+	/**
+	 * @param url the url to set
+	 */
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+}
