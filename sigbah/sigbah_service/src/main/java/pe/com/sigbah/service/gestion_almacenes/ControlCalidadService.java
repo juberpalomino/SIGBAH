0e468293591c2f54e179b7f5564ab51aaa2f4c59
Junior Huaman Flores <hernan.code.zero@gmail.com>
2017-06-19 23:40:48 -0500

    FIX - modulos sigbah.

---
+++ sigbah/sigbah_service/src/main/java/pe/com/sigbah/service/gestion_almacenes/ControlCalidadService.java
@@ -0,0 +1,11 @@
+package pe.com.sigbah.service.gestion_almacenes;
+
+/**
+ * @className: ControlCalidadService.java
+ * @description: 
+ * @date: 19 de jun. de 2017
+ * @author: SUMERIO.
+ */
+public interface ControlCalidadService {
+
+}
---
+++ sigbah/sigbah_service/src/main/java/pe/com/sigbah/service/gestion_almacenes/impl/ControlCalidadServiceImpl.java
@@ -0,0 +1,13 @@
+package pe.com.sigbah.service.gestion_almacenes.impl;
+
+import pe.com.sigbah.service.gestion_almacenes.ControlCalidadService;
+
+/**
+ * @className: ControlCalidadServiceImpl.java
+ * @description: 
+ * @date: 19 de jun. de 2017
+ * @author: SUMERIO.
+ */
+public class ControlCalidadServiceImpl implements ControlCalidadService {
+
+}
