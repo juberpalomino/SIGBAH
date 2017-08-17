b1a7c75282046fb36b6187ea01dfd0f71f84749e
Junior Huaman Flores <hernan.code.zero@gmail.com>
2017-08-16 20:11:57 -0500

    FIX - Estandarizacion Mensajes Validacion Parte 3.

--- sigbah/sigbah_web/src/main/webapp/resources/js/programacion_bah/listar_requerimientos_edan.js
+++ sigbah/sigbah_web/src/main/webapp/resources/js/programacion_bah/listar_requerimientos_edan.js
@@ -51,9 +51,9 @@ $(document).ready(function() {
 		}
 		
 		if (indices.length == 0) {
-			addWarnMessage(null, 'Debe de Seleccionar por lo menos un Registro');
+			addWarnMessage(null, mensajeValidacionSeleccionarRegistro);
 		} else if (indices.length > 1) {
-			addWarnMessage(null, 'Debe de Seleccionar solo un Registro');
+			addWarnMessage(null, mensajeValidacionSeleccionarSoloUnRegistro);
 		} else {
 			loadding(true);
 			var url = VAR_CONTEXT + '/programacion-bath/requerimiento/mantenimientoRequerimiento/';
@@ -81,7 +81,7 @@ $(document).ready(function() {
 			return false;
 		});					
 		if (!esnulo(empty) || row < 1) {
-			addWarnMessage(null, 'No se encuentran registros para generar el reporte.');
+			addWarnMessage(null, mensajeReporteRegistroValidacion);
 			return;
 		}
 
@@ -134,9 +134,9 @@ $(document).ready(function() {
 		}
 		
 		if (indices.length == 0) {
-			addWarnMessage(null, 'Debe de Seleccionar por lo menos un Registro');
+			addWarnMessage(null, mensajeValidacionSeleccionarRegistro);
 		} else if (indices.length > 1) {
-			addWarnMessage(null, 'Debe de Seleccionar solo un Registro');
+			addWarnMessage(null, mensajeValidacionSeleccionarSoloUnRegistro);
 		} else {
 			loadding(true);
 			var url = VAR_CONTEXT + '/programacion-bath/requerimiento/exportarPdf/'+codigo;
@@ -180,9 +180,9 @@ $(document).ready(function() {
 //		}
 //		
 //		if (indices.length == 0) {
-//			addWarnMessage(null, 'Debe de Seleccionar por lo menos un Registro');
+//			addWarnMessage(null, mensajeValidacionSeleccionarRegistro);
 //		} else if (indices.length > 1) {
-//			addWarnMessage(null, 'Debe de Seleccionar solo un Registro');
+//			addWarnMessage(null, mensajeValidacionSeleccionarSoloUnRegistro);
 //		} else {
 //			loadding(true);
 //			var url = VAR_CONTEXT + '/programacion-bath/emergencia/exportarPdf/'+codigo;
